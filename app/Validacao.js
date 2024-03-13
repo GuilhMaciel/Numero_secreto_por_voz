85270d8a7f2d18d65e4f51ef779afc7cbb04f45c
Guilherme Maciel <gui.maciel2001@gmail.com>
2024-03-13 10:06:45 -0300

    validando o chute e mudando o body para acerto

--- app/Validacao.js
+++ app/Validacao.js
@@ -1,3 +1,27 @@
 function verificarValorValido(chute) {
-    
+    const numero = +chute;
+
+    if (chuteInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor Inválido!</div>';
+    }
+
+    if (numeroMaiorOuMenorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>
+        Valor Inválido: Fale um número entre ${menorValor} e ${maiorValor}.</div>`;
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você Acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroMaiorOuMenorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
 }
